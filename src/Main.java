b8936a97c27ff06857d1464f407b8539550cb079
Asim Ahmed <119394951+asim4a@users.noreply.github.com>
2022-11-29 14:48:48 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int a=10;
+        int b=20;
+        int c=5;
+        if (a>b && a>c)
+        {
+        System.out.println("a is greater");
+        }
+        else if(b>a && b>c)
+        {
+         System.out.println("b is greater");
+        }
+        else{
+         System.out.println("c is greater");    
     }
 }
