a99e07e10b0ad568be63d4fb24e1100a99e18764
Raviteja2276 <119394850+Raviteja2276@users.noreply.github.com>
2022-11-29 15:22:21 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         int a=10;
         int b=15;
-        int c=5;
+        int c=90;
         if (a>b && a>c)
         {
         System.out.println("a is greater");
